073fdf04c59561a0176f3fe2d522628b3c50ab2a
Jean Abed <jean.abed@hotmail.com>
2020-12-06 22:48:00 +1100

    testing

--- Formula/neovim-nightly.rb
+++ Formula/neovim-nightly.rb
@@ -4,6 +4,4 @@ class NeovimNightly < Formula
   url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.bz2"
   name "Neovim Nightly"
   homepage "https://neovim.io/"
-
-  binary "nvim-osx64/bin/nvim"
 end
