fbafd85ba81097545bcda38bb6a70179a162e8f0
Jean Abed <jean.abed@hotmail.com>
2020-12-06 22:19:14 +1100

    hotfix for github actions

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -2,7 +2,7 @@ cask 'neovim-nightly' do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz"
+  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.bz2"
   name 'Neovim Nightly'
   homepage 'https://neovim.io/'
 
